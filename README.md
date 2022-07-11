b0a691ff6409dee3d2027269b31454a17852c291
EthanHarp <ethanharp12@protonmail.com>
2022-07-11 15:56:12 -0400

    ROS2 package, should be run inside of a ROS2 workspace

---
+++ README.md
@@ -0,0 +1,9 @@
+Next steps:
+Change from a service to an action for continuous usage
+Create a launch file
+
+
+Libraries used: Pytorch, OpenCV, ROS2, facenet-pytorch
+
+
+For privacy reasons I will not include my dataset or model as that is directly based off of my identity
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'face_recdet'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ethan',
+    maintainer_email='ethan@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'service = face_recdet.recognition:main',
+            'client = face_recdet.detection:main',
+        ],
+    },
+)
